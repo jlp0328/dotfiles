2415c63321a05818bf8c3049b90740ca57fad8c6
Don Denton <don@happycollision.com>
2020-03-30 11:42:42 -0500

    Assume brew first for asdf

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,14 +1,12 @@
 # ensure dotfiles bin directory is loaded first
 PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 
-# Try loading ASDF from the regular home dir location
-if [ -f "$HOME/.asdf/asdf.sh" ]; then
+# Load asdf
+if which brew &>/dev/null; then
+  . "$(brew --prefix asdf)/asdf.sh"
+elif [ -f "$HOME/.asdf/asdf.sh" ]; then
   . "$HOME/.asdf/asdf.sh"
   . "$HOME/.asdf/completions/asdf.bash"
-elif which brew >/dev/null &&
-  BREW_DIR="$(dirname `which brew`)/.." &&
-  [ -f "$BREW_DIR/opt/asdf/asdf.sh" ]; then
-  . "$BREW_DIR/opt/asdf/asdf.sh"
 fi
 
 # mkdir .git/safe in the root of repositories you trust
